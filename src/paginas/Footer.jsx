73d51d04085fa0dbd37292ed8772237c0c28a0f8
VITOR RODRIGUES DO PRADO <vitor.rprado1@senacsp.edu.br>
2024-09-28 13:21:32 -0300

    primeiro-comite

---
+++ src/paginas/Footer.jsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "../assets/css/base/Footer.css"; // Certifique-se de importar o CSS
+
+// Componente Footer para o rodapé
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <p>Filho do Junin Careca</p>
+      </div>
+      <div>
+        <p>Endereço: Parque Centenário</p>
+      </div>
+      <div>
+        <p>Desenvolvido por: Amigo do Filho do Junin Careca</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
